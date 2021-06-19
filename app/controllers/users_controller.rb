1f672697f0ad2a1744211984213596230cce69ee
Aaron Golec <aaron.sescie@gmail.com>
2021-06-19 12:34:01 +0800

    Made edit user path for the current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,21 @@ class UsersController < ApplicationController
   end
 
   def show
+
+  end
+
+  def edit
+    @user = current_user
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :address, :we_chat)
+  end
+
 end
